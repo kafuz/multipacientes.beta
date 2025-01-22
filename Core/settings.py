146cb2839c7673cd23bb8f34a72db36357c9d68c
kafuz <kafu-451@hotmail.com>
2025-01-22 16:48:19 -0500

    whitenoise 2

--- Core/settings.py
+++ Core/settings.py
@@ -65,7 +65,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     'accounts.apps.AccountsConfig',
     'institucionales.apps.InstitucionalesConfig',
     'lobby.apps.LobbyConfig',
